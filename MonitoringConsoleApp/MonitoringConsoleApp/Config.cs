2924f2646554e34c9540b74977c9c2b1b6b10edf
keki228 <nursultan.aldaiarov@gmail.com>
2020-07-03 13:27:53 +0600

    Rewrited on another framework.

---
+++ MonitoringConsoleApp/MonitoringConsoleApp/Config.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MonitoringConsoleApp
+{
+    static class Config
+    {
+        // Enter path which has to be monitored
+        public static readonly string pathToMonitor = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        // Enter path on the server where app has to save files
+        public static readonly string directoryToSave = "/docker/appdata/herbarium/uploads/raw";
+        // Enter your host name or IP here
+        public static readonly string host = @"158.69.52.119";
+        // Enter your port here
+        public static readonly int port = 11022;
+        // Enter your sftp username here
+        public static readonly string username = @"erjan";
+        // Enter your sftp password here
+        public static readonly string password = @"6eYbUb4MuD";
+    }
+}
---
+++ MonitoringConsoleApp/MonitoringConsoleApp/SendFileToServer.cs
@@ -0,0 +1,43 @@
+﻿using Renci.SshNet;
+using System;
+
+namespace MonitoringConsoleApp
+{
+    public static class SendFileToServer
+    {
+        // Enter your host name or IP here
+        private static string host = Config.host;
+        // Enter your port here
+        private static int port = Config.port;
+        // Enter your sftp username here
+        private static string username = Config.username;
+        // Enter your sftp password here
+        private static readonly string password = Config.password;
+        public static int Send(string fileName)
+        {
+            try
+            {
+                //var connectionInfo = new ConnectionInfo(host, "erjan", new PasswordAuthenticationMethod(username, password));
+                // Upload File
+                using (var sftp = new SftpClient(host, port, username, password))
+                {
+
+                    sftp.Connect();
+                    sftp.ChangeDirectory(Config.directoryToSave);
+                    using (var uplfileStream = System.IO.File.OpenRead(fileName))
+                    {
+                        sftp.UploadFile(uplfileStream, fileName, true);
+                    }
+                    sftp.Disconnect();
+                    Console.WriteLine("\n\n" + fileName + " uploaded successfully.\n");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Console.WriteLine("\n\n" + fileName + " was not uploaded.\n\n");
+            }
+            return 0;
+        }
+    }
+}
